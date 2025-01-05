27b65c18beb11416fec637efceb84800b054404e
rednt <47644820+rednt@users.noreply.github.com>
2025-01-05 18:32:54 +0100

    side bar and image animation on scroll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const container = document.getElementById('container');
+
+
+/*const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
@@ -8,4 +10,32 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
-});
+});*/
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    const images = document.querySelectorAll('.image img');
+    images.forEach((image) => {
+        if (isInViewport(image)) {
+            image.classList.add('visible');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
+
+window.addEventListener('DOMContentLoaded', handleScroll);
+
+
+
+
